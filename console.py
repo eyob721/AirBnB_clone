97f5efe4baeacd0fec800f9cd2e5cdf663db931e
eyob721 <eyobmelkamu721@gmail.com>
2024-03-19 11:30:01 +0300

    docs: organize documentation and help sections

--- console.py
+++ console.py
@@ -1,25 +1,41 @@
 #!/usr/bin/python3
-"""AirBnB clone - console"""
+"""AirBnB clone - console
+
+This is the entry point of the command interpreter
+
+"""
 import cmd
 
+
 class HBNBCommand(cmd.Cmd):
-    
+    """Class definition of the AirBnB clone - console"""
+
     prompt = "(hbnb) "
-    
+
+    # COMMAND handlers
+
     def do_quit(self, args):
+        """Handler for the quit command"""
         return True
 
     def do_EOF(self, args):
+        """Handler for end-of-file signal"""
         return True
 
     def emptyline(self):
+        """Handler for empty line + ENTER"""
         pass
 
-    def help_quit(self, args):
-        print("Quit the console")
+    # HELP handlers
 
-    def help_EOF(self, args):
-        print("Handles EOF")    
-    
-if __name__ == '__main__':
+    def help_quit(self):
+        """Help for the quit command"""
+        print("Usage: quit\n" + "Quits from the console")
+
+    def help_EOF(self):
+        """Help for the EOF command"""
+        print("Usage: EOF\n" + "Quits from the console")
+
+
+if __name__ == "__main__":
     HBNBCommand().cmdloop()
