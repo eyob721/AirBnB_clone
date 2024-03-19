619d3bafa916f788dc19ee6ab6693d810c6b294b
Ezralx <ezrateshome.alx@gmail.com>
2024-03-19 23:24:49 +0300

    feat: implement the show command

--- console.py
+++ console.py
@@ -50,6 +50,36 @@ class HBNBCommand(cmd.Cmd):
         storage.save()
         print(obj.id)
 
+    def  do_show(self, args):
+        """Handler for the show command"""
+        
+        # Check if class name is given
+        if not args:
+            print("** class name missing **")
+            return
+        
+        # Check if given class name exist
+        class_name = args.split(" ",1)[0]
+        if class_name not in self.__valid_classes:
+            print("** class doesn't exist **")
+            return
+        
+        # Check if id is given
+        if len(args.split(" ")) == 1:
+            print("** instance id missing **")
+            return
+        
+        # If id is given, check instance exists
+        id = args.split(" ", 2)[1]
+        key = f"{class_name}.{id}"
+        if key not in storage.all():
+            print("** no instance found **")
+            return
+
+        # If instance exists, print it's string representation
+        obj = storage.all()[key]
+        print(obj)
+        
     # HELP handlers
 
     def help_quit(self):
