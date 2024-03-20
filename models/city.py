668cdfc765b233bf8e722d480100a65e9e5747b1
Ezralx <ezrateshome.alx@gmail.com>
2024-03-20 22:45:32 +0300

    feat: implement the city class

---
+++ models/city.py
@@ -0,0 +1,10 @@
+#!/user/bin/python3
+"""we implement city class that inherites from BaseModel"""
+import cmd
+from models.base_model import BaseModel
+
+class state(BaseModel):
+    """" Class definition of City class"""
+    
+    state_id = ""
+    name = ""
