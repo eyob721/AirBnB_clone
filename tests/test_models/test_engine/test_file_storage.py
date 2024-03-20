f91c9848e92a70a3daa12232571d0d705ad4e8d9
eyob721 <eyobmelkamu721@gmail.com>
2024-03-20 23:50:12 +0300

    test: update tests to handel State, City, Amenity, Place and Review objects

--- tests/test_models/test_engine/test_file_storage.py
+++ tests/test_models/test_engine/test_file_storage.py
@@ -6,8 +6,13 @@ from datetime import datetime, timedelta
 from unittest import TestCase
 from uuid import uuid4
 
+from models.amenity import Amenity
 from models.base_model import BaseModel
+from models.city import City
 from models.engine.file_storage import FileStorage
+from models.place import Place
+from models.review import Review
+from models.state import State
 from models.user import User
 
 
@@ -16,7 +21,15 @@ def remove_file(filepath):
         os.remove(filepath)
 
 
-valid_classes = ("BaseModel", "User")
+valid_classes = (
+    "Amenity",
+    "BaseModel",
+    "City",
+    "Place",
+    "Review",
+    "State",
+    "User",
+)
 
 
 class TestFileStorageClass(TestCase):
