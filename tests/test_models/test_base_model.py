81f1254464ead5194036241307c96f11164658ef
eyob721 <eyobmelkamu721@gmail.com>
2024-03-20 17:50:36 +0300

    chore: organize test files

--- tests/test_models/test_base_model.py
+++ tests/test_models/test_base_model.py
@@ -11,10 +11,12 @@ class TestBaseModelClass(TestCase):
     """Tests for the BaseModel class"""
 
     def test_instance_type(self):
+        """Check that instance of BaseModel is instance of BaseModel"""
         b = BaseModel()
         self.assertTrue(type(b) is BaseModel)
 
     def test_instantiation_with_kwargs(self):
+        """Check creating a BaseModel object from a dictionary"""
         b1 = BaseModel()
         b1.program = "Alx"  # pyright: ignore
         b1.cohort = 21  # pyright: ignore
@@ -29,12 +31,14 @@ class TestBaseModelAttributes(TestCase):
     """Tests for the attributes of BaseModel"""
 
     def test_attributes_exist(self):
+        """Check that the right attributes exist"""
         valid_attr = ["id", "created_at", "updated_at"]
         b = BaseModel()
         for attr in valid_attr:
             self.assertTrue(attr in dir(b))
 
     def test_id_attribute(self):
+        """Check type and value of the id attribute"""
         b = BaseModel()
 
         # Check type of id
@@ -44,51 +48,41 @@ class TestBaseModelAttributes(TestCase):
         self.assertRegex(b.id, r"^\w+-\w+-\w+-\w+-\w+$")
 
     def test_datetime_attributes_at_instantiation(self):
+        """Check type and value of the attributes, created_at and updated_at"""
         current_time = datetime.now()
         b = BaseModel()
 
         # Check created_at attribute type
         self.assertIs(type(b.created_at), datetime)
 
-        # Check created_at attribute value
+        # Check created_at attribute value == current date and time
         self.assertAlmostEqual(
-            current_time,
-            b.created_at,
-            delta=timedelta(seconds=1),
-            msg="created_at value is not the current datetime",
+            current_time, b.created_at, delta=timedelta(seconds=1)
         )
 
         # Check updated_at attribute type
         self.assertIs(type(b.updated_at), datetime)
 
-        # Check updated_at attribute value
-        self.assertEqual(
-            b.created_at,
-            b.updated_at,
-            msg="updated_at != created_at at instantiation",
-        )
+        # Check updated_at attribute value == created_at
+        self.assertEqual(b.created_at, b.updated_at)
 
 
 class TestBaseModelSaveMethod(TestCase):
     """Tests for the save method of BaseModel"""
 
     def test_save_method_is_defined(self):
-        self.assertTrue(
-            "save" in dir(BaseModel()), msg="save method is not defined"
-        )
+        """Check that the save method is defined"""
+        self.assertTrue("save" in dir(BaseModel()))
 
     def test_save_method_works_correctly(self):
-        # Check that the updated_at attribute is updated upon save
+        """Check that the BaseModel save method works correctly"""
+        # Check that the updated_at attribute is updated with current datetime
         b = BaseModel()
         sleep(3)
         current_time = datetime.now()
         b.save()
         self.assertAlmostEqual(
-            b.updated_at,
-            current_time,
-            delta=timedelta(seconds=1),
-            msg="updated_at attributte is not updated with "
-            + "current datetime upon save",
+            b.updated_at, current_time, delta=timedelta(seconds=1)
         )
 
 
@@ -96,11 +90,11 @@ class TestBaseModelTo_DictMethod(TestCase):
     """Tests for the to_dict method of BaseModel"""
 
     def test_to_dict_method_is_defined(self):
-        self.assertTrue(
-            "to_dict" in dir(BaseModel()), msg="to_dict method is not defined"
-        )
+        """Check that the to_dict method is defined"""
+        self.assertTrue("to_dict" in dir(BaseModel()))
 
     def test_to_dict_method_return_value(self):
+        """Check the contents of the returned dictionary"""
         b = BaseModel()
         valid_keys = ["__class__", "id", "created_at", "updated_at"]
         b_dict = b.to_dict()
