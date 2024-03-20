f64f8d20f5b5bcaf0e09961519d88091adfd637a
Ezralx <ezrateshome.alx@gmail.com>
2024-03-20 22:39:16 +0300

    feat: implement the state class

---
+++ models/state.py
@@ -0,0 +1,9 @@
+#!/user/bin/python3
+"""we implement city class that inherites from BaseModel"""
+import cmd
+from models.base_model import BaseModel
+
+class state(BaseModel):
+    """"Contain city class"""
+    
+    name = ""
